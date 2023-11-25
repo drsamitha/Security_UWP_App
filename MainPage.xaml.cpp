60aed2c1b1cd896e9b42e173b2f28664ee984b07
SAM <SAM@DESKTOP-P6FHT2F>
2023-11-25 23:52:43 +0530

    win track control fn NO WORK NO ERROR

--- MainPage.xaml.cpp
+++ MainPage.xaml.cpp
@@ -25,13 +25,15 @@ using namespace Windows::UI::Xaml::Navigation;
 
 MainPage::MainPage()
 {
-	InitializeComponent();
+    InitializeComponent();
 
+    localSettings = Windows::Storage::ApplicationData::Current->LocalSettings;
 }
+
 void MainPage::Tool1_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
 {
     // Your code here for Tool 1
-        // Create a new ContentDialog
+    // Create a new ContentDialog
     ContentDialog^ dialog = ref new ContentDialog();
 
     // Create a new ToggleSwitch
@@ -49,6 +51,24 @@ void MainPage::Tool1_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEv
 
     // Show the dialog
     dialog->ShowAsync();
+
+    // Save the state of the ToggleSwitch
+    bool m_toggleSwitchState = toggleSwitch->IsOn;
+}
+
+#include <fstream>
+#include <string>
+
+void blockIPsFromFile(const std::string& filename)
+{
+    std::ifstream file(filename);
+    std::string line;
+
+    while (std::getline(file, line))
+    {
+        std::string command = "netsh advfirewall firewall add rule name=\"Block " + line + "\" dir=in interface=any action=block remoteip=" + line;
+        system(command.c_str());
+    }
 }
 
 void MainPage::Tool2_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
@@ -56,43 +76,64 @@ void MainPage::Tool2_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEv
     // Create a new ContentDialog
     ContentDialog^ dialog = ref new ContentDialog();
 
-    // Create a new StackPanel to hold the toggle switches
-    StackPanel^ switchPanel = ref new StackPanel();
-    switchPanel->Orientation = Orientation::Vertical;
+    // Create a new ComboBox
+    ComboBox^ comboBox = ref new ComboBox();
+    comboBox->Items->Append("None");
+    comboBox->Items->Append("Item 2");
+    comboBox->Items->Append("Item 3");
 
-    // Create a new ToggleSwitch
-    ToggleSwitch^ toggleSwitch = ref new ToggleSwitch();
-    toggleSwitch->Header = "Toggle Switch";
-    toggleSwitch->OffContent = "Off";
-    toggleSwitch->OnContent = "On";
-
-    // Add the toggle switch to the switch panel
-    switchPanel->Children->Append(toggleSwitch);
-
-    // Create two more toggle switches with different headers
-    ToggleSwitch^ toggleSwitch1 = ref new ToggleSwitch();
-    toggleSwitch1->Header = "Toggle Switch 1";
-    toggleSwitch1->OffContent = "Off";
-    toggleSwitch1->OnContent = "On";
-
-    ToggleSwitch^ toggleSwitch2 = ref new ToggleSwitch();
-    toggleSwitch2->Header = "Toggle Switch 2";
-    toggleSwitch2->OffContent = "Off";
-    toggleSwitch2->OnContent = "On";
-
-    // Add the toggle switches to the switch panel
-    switchPanel->Children->Append(toggleSwitch1);
-    switchPanel->Children->Append(toggleSwitch2);
+    // Set the selected item to the stored item
+    if (m_selectedItem != nullptr)
+    {
+        comboBox->SelectedItem = m_selectedItem;
+    }
 
     // Set the properties of the dialog
     dialog->Title = "Tool 2";
-    dialog->Content = switchPanel; // Set the content to the switch panel
+    dialog->Content = comboBox; // Set the content to the ComboBox
     dialog->CloseButtonText = "OK";
 
     // Show the dialog
     dialog->ShowAsync();
+
+
+
+    // Handle the selected item
+    if (comboBox->SelectedItem != nullptr)
+    {
+        Platform::String^ selectedItem = safe_cast<Platform::String^>(comboBox->SelectedItem);
+
+        if (selectedItem == "None")
+        {
+            // Code to make default IP rules
+            #include <cstdlib>
+
+            void resetIPRules();
+            {
+                system("netsh advfirewall reset");
+            }
+
+
+        }
+        else if (selectedItem == "Item 2")
+        {
+            // Code to block IP from "ip_list_1.txt"
+            blockIPsFromFile("ip_list_1.txt");
+        }
+        else if (selectedItem == "Item 3")
+        {
+            // Code to block IP from "ip_list_1.txt" and "ip_list_2.txt"
+            blockIPsFromFile("ip_list_1.txt");
+            blockIPsFromFile("ip_list_2.txt");
+        }
+    }
 }
 
+void MainPage::ComboBox_SelectionChanged(Platform::Object^ sender, Windows::UI::Xaml::Controls::SelectionChangedEventArgs^ e)
+{
+    ComboBox^ comboBox = safe_cast<ComboBox^>(sender);
+    m_selectedItem = safe_cast<Platform::String^>(comboBox->SelectedItem);
+}
 
 
 
--- MainPage.xaml.h
+++ MainPage.xaml.h
@@ -14,13 +14,40 @@ namespace App1
 	/// </summary>
     public ref class MainPage sealed
     {
-    public:
-        MainPage();
+    private:
+        Platform::String^ m_selectedItem;
+        Windows::Storage::ApplicationDataContainer^ localSettings;
+       
+
+public:
+    MainPage();
+
+
         void Tool1_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
         void Tool2_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+        void ComboBox_SelectionChanged(Platform::Object^ sender, Windows::UI::Xaml::Controls::SelectionChangedEventArgs^ e);
         void Tool3_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
         void Tool4_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
         void Tool5_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+
+        void OnNavigatedFrom(Windows::UI::Xaml::Navigation::NavigatingCancelEventArgs^ e)
+        {
+            // Save the state of your controls here
+            localSettings->Values->Insert("selectedItem", dynamic_cast<Platform::Object^>(m_selectedItem));
+        }
+
+        void OnNavigatedTo(Windows::UI::Xaml::Navigation::NavigatingCancelEventArgs^ e)
+        {
+            // Restore the state of your controls here
+            if (localSettings->Values->HasKey("selectedItem"))
+            {
+                m_selectedItem = dynamic_cast<Platform::String^>(localSettings->Values->Lookup("selectedItem"));
+            }
+        }
     };
+
+
+
+
 
 }
